16ce0737db6a501682a3226c3b8b365e1d656d62
Ichijo Hiroki <hiroki.ichijo@axas-japan.co.jp>
2022-05-30 22:16:24 +0900

    Initial Commit

---
+++ HotPepperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotPepperApp
+//
+//  Created by AXLT0221-AP on 2022/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
